1ef0f8642a48f69d504a3fd72e9a411c23761333
Shlok Bhakta <shlokbhakta1@gmail.com>
2022-03-31 14:38:44 -0500

    first commit

---
+++ src/App.java
@@ -0,0 +1,21 @@
+public class App {
+    //vscode is an editor
+    public static void main(String[] args) throws Exception {
+        //start
+        double joels_Height = 15.090897 - 80.0007;
+
+
+        System.out.println(joels_Height);
+
+        //end
+    }
+}
+/*         // <Data Type> Name = <Value>;
+        
+        int a = 0; //Integer 1 2 3 4 5 6 7 8 9 10   int != 8.3
+        String b = "oogabooga"; //lksfkjshfkjshgfkjewhfkjehf Text
+        double c = 0;   // big decimal stuff like 1.2 9.85467827734682364  6.2546
+/*         long d = 0; // smaller decimal stuff like 1.2 9.85467827
+        char e = 'o';   // a singular character a b o j 4 X x dont rlly use this
+        byte f  = 0;    //  very smol number dont rlly use
+        short g = 0;    // bigger than byte smaller than long dont rlly use this one  */ 
