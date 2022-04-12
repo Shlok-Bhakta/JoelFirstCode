1b52595344b60a12df7793079ee5ac36305c50a6
Shlok Bhakta <shlokbhakta1@gmail.com>
2022-04-12 14:26:42 -0500

    guess game with swing stuff

--- src/App.java
+++ src/App.java
@@ -1,21 +1,37 @@
+
+//learn about packages
+import java.util.*;
+import javax.swing.*;
+//only use import above the class
 public class App {
-    //vscode is an editor
+    // vscode is an editor
     public static void main(String[] args) throws Exception {
-        //start
-        double joels_Height = 15.090897 - 80.0007;
-
-
-        System.out.println(joels_Height);
-
-        //end
-    }
-}
-/*         // <Data Type> Name = <Value>;
+        // Objective - have the user enter a number between 1 and 10 and if they are
+        // correct to the random mumber than they win
+    
         
-        int a = 0; //Integer 1 2 3 4 5 6 7 8 9 10   int != 8.3
-        String b = "oogabooga"; //lksfkjshfkjshgfkjewhfkjehf Text
-        double c = 0;   // big decimal stuff like 1.2 9.85467827734682364  6.2546
-/*         long d = 0; // smaller decimal stuff like 1.2 9.85467827
-        char e = 'o';   // a singular character a b o j 4 X x dont rlly use this
-        byte f  = 0;    //  very smol number dont rlly use
-        short g = 0;    // bigger than byte smaller than long dont rlly use this one  */ 
+        int min = 1;
+        int max = 10;
+        int random_int = (int)Math.floor(Math.random()*(max-min+1)+min);
+
+        // learn about java.util.Scanner for user input
+        Scanner User = new Scanner(System.in);
+        
+        //set to a variable number
+        int Vari = Integer.parseInt(JOptionPane.showInputDialog(null, "Enter a number between 1 and 10 >> ", "number", JOptionPane.QUESTION_MESSAGE));
+
+
+        // learn if statements
+        if(Vari == random_int){
+         
+         JOptionPane.showMessageDialog(null, "Helooo You wan. Robot picked: " + random_int, "Woo", JOptionPane.INFORMATION_MESSAGE);
+        }else{
+            
+            JOptionPane.showMessageDialog(null, "Helooo nyou suck. Robot picked: " + random_int, "Woo", JOptionPane.INFORMATION_MESSAGE);
+            JOptionPane.showMessageDialog(null, "you are bad at guessing go away noob: " + random_int, "woo", JOptionPane.INFORMATION_MESSAGE);
+        }
+
+    }
+    }
+
+
